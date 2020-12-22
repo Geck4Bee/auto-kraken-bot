77c379e8339180ff9212a0e98fc233bb91f978d9
geck4bee <geck4bee@gmail.com>
2020-12-22 22:41:47 +0000

    Setup Selenium

--- index.py
+++ index.py
@@ -6,31 +6,53 @@ import datetime
 
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
 
+def webscrape():
+    # webdriver settings
+    try:
+        options = webdriver.ChromeOptions()
+        options.binary_location = "./bin/headless-chromium"
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--single-process")
+    
+        driver = webdriver.Chrome(
+            executable_path="./bin/chromedriver",
+            chrome_options=options
+        )
+    
+        # web scraping
+        driver.get("https://kraken-wood.com/")
+        #ここの時間が長いとlambdaの料金が無駄にかかります。driverがページの表示にかかる時間分だけを設定するのが良いです。
+        time.sleep(3) 
+        html = driver.page_source.encode('utf-8')
+    except Exception as ee:
+        sys.stderr.write("*** error *** in webdriver ***\n")
+        sys.stderr.write(str(ee) + "\n")
+    else:
+        return shaping_data(html)
+    
+def shaping_data(html):
+    objs = []
+    try:
+        soup = BeautifulSoup(html, "html.parser")
+        for aa in soup.find_all("a"):
+            link = aa.get("href")
+            name = aa.get_text()
+            objs.append({
+                'link': link,
+                'name': name
+            })
+    except Exception as ee:
+        sys.stderr.write("*** error *** in BeautifulSoup ***\n")
+        sys.stderr.write(str(ee) + "\n")
+    else:
+        return objs
 
 def handler(event, context):
-    try:
-        # スクレイピング対象の URL にリクエストを送り HTML を取得する
-        headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0",}
-        res = requests.get('https://kraken-wood.com/', headers=headers)
-        
-        objs = []
-        try:
-            soup = BeautifulSoup(res.text, "html.parser")
-            for aa in soup.find_all("a"):
-                link = aa.get("href")
-                name = aa.get_text()
-                objs.append({
-                    'link': link,
-                    'name': name
-                })
-        except Exception as ee:
-            sys.stderr.write("*** error *** in BeautifulSoup ***\n")
-            sys.stderr.write(str(ee) + "\n")
-    except Exception as ee:
-        sys.stderr.write("*** error *** in requests.get ***\n")
-        sys.stderr.write(str(ee) + "\n")
-    
+    objs = webscrape()
     data = {
         'output': 'Hello World',
         'timestamp': datetime.datetime.utcnow().isoformat(),
