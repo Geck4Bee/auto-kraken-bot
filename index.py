f28d1ff3a29c9202f96fc7ab143a0d808215c67a
geck4bee <geck4bee@gmail.com>
2020-12-22 15:39:10 +0000

    First Auto Kraken

--- index.py
+++ index.py
@@ -9,33 +9,32 @@ from bs4 import BeautifulSoup
 
 
 def handler(event, context):
-    # スクレイピング対象の URL にリクエストを送り HTML を取得する
-    res = requests.get('http://quotes.toscrape.com/')
-    
-    # レスポンスの HTML から BeautifulSoup オブジェクトを作る
-    soup = BeautifulSoup(res.text, 'html.parser')
-    
-    # title タグの文字列を取得する
-    title_text = soup.find('title').get_text()
-    print(title_text)
-    # > Quotes to Scrape
-    
-    # ページに含まれるリンクを全て取得する
-    links = [url.get('href') for url in soup.find_all('a')]
-    print(links)
-    # > ['/', '/login', '/author/Albert-Einstein', '/tag/change/page/1/', '/tag/deep-thoughts/page/1/', '/tag/thinking/page/1/', '/tag/world/page/1/', '/author/J-K-Rowling', '/tag/abilities/page/1/', '/tag/choices/page/1/', '/author/Albert-Einstein', '/tag/inspirational/page/1/', '/tag/life/page/1/', '/tag/live/page/1/', '/tag/miracle/page/1/', '/tag/miracles/page/1/', '/author/Jane-Austen', '/tag/aliteracy/page/1/', '/tag/books/page/1/', '/tag/classic/page/1/', '/tag/humor/page/1/', '/author/Marilyn-Monroe', '/tag/be-yourself/page/1/', '/tag/inspirational/page/1/', '/author/Albert-Einstein', '/tag/adulthood/page/1/', '/tag/success/page/1/', '/tag/value/page/1/', '/author/Andre-Gide', '/tag/life/page/1/', '/tag/love/page/1/', '/author/Thomas-A-Edison', '/tag/edison/page/1/', '/tag/failure/page/1/', '/tag/inspirational/page/1/', '/tag/paraphrased/page/1/', '/author/Eleanor-Roosevelt', '/tag/misattributed-eleanor-roosevelt/page/1/', '/author/Steve-Martin', '/tag/humor/page/1/', '/tag/obvious/page/1/', '/tag/simile/page/1/', '/page/2/', '/tag/love/', '/tag/inspirational/', '/tag/life/', '/tag/humor/', '/tag/books/', '/tag/reading/', '/tag/friendship/', '/tag/friends/', '/tag/truth/', '/tag/simile/', 'https://www.goodreads.com/quotes', 'https://scrapinghub.com']
-    
-    # class が quote の div 要素を全て取得する
-    quote_elms = soup.find_all('div', {'class': 'quote'})
-    print(len(quote_elms))
-    # > 10
+    try:
+        # スクレイピング対象の URL にリクエストを送り HTML を取得する
+        headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0",}
+        res = requests.get('https://kraken-wood.com/', headers=headers)
+        
+        objs = []
+        try:
+            soup = BeautifulSoup(res.content, "html.parser")
+            for aa in soup.find_all("a"):
+                link = aa.get("href")
+                name = aa.get_text()
+                objs.append({
+                    'link': link,
+                    'name': name
+                })
+        except Exception as ee:
+            sys.stderr.write("*** error *** in BeautifulSoup ***\n")
+            sys.stderr.write(str(ee) + "\n")
+    except Exception as ee:
+        sys.stderr.write("*** error *** in requests.get ***\n")
+        sys.stderr.write(str(ee) + "\n")
     
     data = {
         'output': 'Hello World',
         'timestamp': datetime.datetime.utcnow().isoformat(),
-        'title': title_text,
-        'links': ", ".join(links),
-        'quote': quote_elms
+        'objs': json.dumps(objs)
     }
 
     return {'statusCode': 200,
