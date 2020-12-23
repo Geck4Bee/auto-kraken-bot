e7a3bf21c282665cbf3e49aaed28cbc0b195b520
geck4bee <geck4bee@gmail.com>
2020-12-23 04:45:21 +0000

    Translate and Read&Put DynamoDB

--- index.py
+++ index.py
@@ -1,13 +1,20 @@
 import sys
 sys.path.append('lib')
 
+import os
 import json
 import datetime
+import boto3
+from boto3.dynamodb.conditions import Key
 
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+
+translate = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
+dynamoDB = boto3.resource('dynamodb', 'ap-northeast-1')
+table = dynamoDB.Table(os.environ['TABLE'])
 
 def webscrape():
     # webdriver settings
@@ -42,8 +49,9 @@ def shaping_data(html):
             link = aa.get("href")
             name = aa.get_text()
             objs.append({
-                'link': link,
-                'name': name
+                'URL': link,
+                'en': name,
+                'ja': '',
             })
     except Exception as ee:
         sys.stderr.write("*** error *** in BeautifulSoup ***\n")
@@ -51,12 +59,68 @@ def shaping_data(html):
     else:
         return objs
 
+def getFromDynamoDB(URL):
+    try:
+        result = table.query(
+            IndexName='byURL',
+            KeyConditionExpression = Key('URL').eq(URL),
+            ScanIndexForward = False,
+            Limit = 1
+        )
+    except Exception as eg:
+        sys.stderr.write("*** error *** in GetFromDynamoDB ***\n")
+        sys.stderr.write(str(eg) + "\n")
+    else:
+        return result
+
+def putDynamoDB(obj):
+    try:
+        table.put_item(
+            Item = {
+                'id': 'test',
+                'URL': obj['URL'],
+                'en': obj['en'],
+                'ja': obj['ja']
+            }
+        )
+    except Exception as ep:
+        sys.stderr.write("*** error *** in PutDynamoDB ***\n")
+        sys.stderr.write(str(ep) + "\n")
+    else:
+        return True
+
+def translation(obj):
+    try:
+        result = translate.translate_text(
+            Text=obj['en'], 
+            SourceLanguageCode="en",
+            TargetLanguageCode="ja"
+        )
+        obj['ja'] = result.get('TranslatedText')
+    except Exception as et:
+        sys.stderr.write("*** error *** in Translation ***\n")
+        sys.stderr.write(str(et) + "\n")
+    else:
+        return obj
+
 def handler(event, context):
     objs = webscrape()
+    translated_objs = []
+    try:
+        for obj in objs:
+            getFromDB = getFromDynamoDB(obj['URL'])
+            if len(getFromDB['Items']) == 0:
+                translated = translation(obj)
+                translated_objs.append(translated)
+                putting = putDynamoDB(obj)
+    except Exception as eh:
+        sys.stderr.write("*** error *** in PutDynamoDB ***\n")
+        sys.stderr.write(str(eh) + "\n")
+
     data = {
         'output': 'Hello World',
         'timestamp': datetime.datetime.utcnow().isoformat(),
-        'objs': json.dumps(objs)
+        'objs': json.dumps(translated_objs)
     }
 
     return {'statusCode': 200,
